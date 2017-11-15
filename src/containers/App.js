8ab8f706d95a1296129f79d66f475dcd74c88afa
Ernestas Paznekas <ernestas.paznekas@gmail.com>
2017-11-15 20:53:14 +0200

    Structure update

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import MainLayout from './layouts/MainLayout'
+import MainLayout from '../layouts/MainLayout'
 
 class App extends Component {
   render() {
